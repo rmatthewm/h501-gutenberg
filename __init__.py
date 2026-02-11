a23feb9299f6dcd2d2dc8e67c4eccba9ee9fcf2d
Chiragk5699 <ckarachi@iu.edu>
2026-02-11 22:42:17 +0000

    Gutenberg 2nd commit

--- __init__.py
+++ __init__.py
@@ -4,3 +4,10 @@ gutenberg_authors = pd.read_csv('https://raw.githubusercontent.com/rfordatascien
 gutenberg_languages = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_languages.csv')
 gutenberg_metadata = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_metadata.csv')
 gutenberg_subjects = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_subjects.csv')
+
+df = pd.merge(gutenberg_languages, gutenberg_metadata, by='gutenberg_id')
+df = pd.merge(df, gutenberg_authors, by='gutenberg_author_id')
+
+print (df.head())
+
+
