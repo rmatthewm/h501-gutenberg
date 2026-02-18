61b6e4ba520f3573cec8d727e5262125df192bd5
mosleym <mosleym@iu.edu>
2026-02-17 20:42:04 -0500

    finished authors module

--- tt_gutenberg/__init__.py
+++ tt_gutenberg/__init__.py
@@ -8,14 +8,28 @@ LANGUAGES_FILE = 'gb_languages'
 METADATA_FILE = 'gb_metadata'
 SUBJECTS_FILE = 'gb_subjects'
 
+# Load the data, if not saved locally, save it to reduce runtime in the future
 if os.path.isfile(f'{AUTHORS_FILE}.csv'):
-    gutenberg_authors = pd.read(f'{AUTHORS_FILE}.csv')
+    gutenberg_authors = pd.read_csv(f'{AUTHORS_FILE}.csv')
 else:
     gutenberg_authors = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_authors.csv')
     gutenberg_authors.to_csv(f'{AUTHORS_FILE}.csv')
 
+if os.path.isfile(f'{LANGUAGES_FILE}.csv'):
+    gutenberg_languages = pd.read_csv(f'{LANGUAGES_FILE}.csv')
+else:
+    gutenberg_languages = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_languages.csv')
+    gutenberg_languages.to_csv(f'{LANGUAGES_FILE}.csv', index=False)
 
-gutenberg_languages = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_languages.csv')
-gutenberg_metadata = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_metadata.csv')
-gutenberg_subjects = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_subjects.csv')
+if os.path.isfile(f'{METADATA_FILE}.csv'):
+    gutenberg_metadata = pd.read_csv(f'{METADATA_FILE}.csv')
+else:
+    gutenberg_metadata = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_metadata.csv')
+    gutenberg_metadata.to_csv(f'{METADATA_FILE}.csv', index=False)
+
+if os.path.isfile(f'{SUBJECTS_FILE}.csv'):
+    gutenberg_subjects = pd.read_csv(f'{SUBJECTS_FILE}.csv')
+else:
+    gutenberg_subjects = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_subjects.csv')
+    gutenberg_subjects.to_csv(f'{SUBJECTS_FILE}.csv', index=False)
 
--- tt_gutenberg/authors.py
+++ tt_gutenberg/authors.py
@@ -3,14 +3,30 @@ import data_merge
 
 
 def list_authors(by_languages=False, by_alias=False):
+    """ Returns the authors in the Gutenberg database as a list
+
+    Args:
+        by_languages (bool, optional): sorts the authors by the number of 
+            languages their works are in. Defaults to False.
+        by_alias (bool, optional): returns all the authors' aliases instead of just their name. Defaults to False.
+
+    Returns:
+        list(str): the list of authors
+    """
+
     # Get the merged data
     df_gutenberg = data_merge.get_merged_data()
+    not_na = df_gutenberg.notna()
 
-    # Sort the authors by language if requested
+    # Sort the authors by language if requested, then by alias
     if by_languages:
-        df_gutenberg.sort_values(by='total_languages')
-        print(df_gutenberg.groupby('gutenberg_author_id')['author_x'])
+        if by_alias:
+            return list(df_gutenberg[not_na].sort_values(by='total_languages')['aliases'].unique())
+        else:
+            return list(df_gutenberg[not_na].sort_values(by='total_languages')['author_x'].unique())
+    else:
 
-    print(list(df_gutenberg['author_x']))
-
-list_authors(by_languages=True)    
+        if by_alias:
+            return list(df_gutenberg[not_na]['aliases'].unique())
+        else:
+            return list(df_gutenberg[not_na]['author_x'].unique())
