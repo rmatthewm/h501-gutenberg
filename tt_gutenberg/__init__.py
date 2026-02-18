433d4bd10978d24f9c92727e40ccbcaa66f607c1
mosleym <mosleym@iu.edu>
2026-02-17 20:09:21 -0500

    saving

--- tt_gutenberg/__init__.py
+++ tt_gutenberg/__init__.py
@@ -1,6 +1,20 @@
 import pandas as pd
+import os
 
-gutenberg_authors = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_authors.csv')
+# Save time for testing by storing the data locally
+# Filenames
+AUTHORS_FILE = 'gb_authors'
+LANGUAGES_FILE = 'gb_languages'
+METADATA_FILE = 'gb_metadata'
+SUBJECTS_FILE = 'gb_subjects'
+
+if os.path.isfile(f'{AUTHORS_FILE}.csv'):
+    gutenberg_authors = pd.read(f'{AUTHORS_FILE}.csv')
+else:
+    gutenberg_authors = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_authors.csv')
+    gutenberg_authors.to_csv(f'{AUTHORS_FILE}.csv')
+
+
 gutenberg_languages = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_languages.csv')
 gutenberg_metadata = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_metadata.csv')
 gutenberg_subjects = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_subjects.csv')
--- tt_gutenberg/authors.py
+++ tt_gutenberg/authors.py
@@ -9,6 +9,8 @@ def list_authors(by_languages=False, by_alias=False):
     # Sort the authors by language if requested
     if by_languages:
         df_gutenberg.sort_values(by='total_languages')
-        print(df_gutenberg['author_x'].head())
+        print(df_gutenberg.groupby('gutenberg_author_id')['author_x'])
+
+    print(list(df_gutenberg['author_x']))
 
 list_authors(by_languages=True)    
