755b7de2c5cc15323d763abd721b2319e8336aa1
mosleym <mosleym@iu.edu>
2026-02-17 21:16:29 -0500

    finished exercise 3

--- tt_gutenberg/authors.py
+++ tt_gutenberg/authors.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from tt_gutenberg.data_merge import *
 
+
 def list_authors(by_languages=False, alias=False):
     """ Returns the authors in the Gutenberg database as a list
 
@@ -15,17 +16,18 @@ def list_authors(by_languages=False, alias=False):
 
     # Get the merged data
     df_gutenberg = get_merged_data()
-    not_na = df_gutenberg.notna()
+    alias_not_na = df_gutenberg['alias'].notna()
+    author_not_na = df_gutenberg['author_x'].notna()
 
     # Sort the authors by language if requested, then by alias
     if by_languages:
         if alias:
-            return list(df_gutenberg[not_na].sort_values(by='total_languages')['aliases'].unique())
+            return list(df_gutenberg[alias_not_na].sort_values(by='total_languages')['aliases'].unique())
         else:
-            return list(df_gutenberg[not_na].sort_values(by='total_languages')['author_x'].unique())
+            return list(df_gutenberg[author_not_na].sort_values(by='total_languages')['author_x'].unique())
     else:
 
         if alias:
-            return list(df_gutenberg[not_na]['aliases'].unique())
+            return list(df_gutenberg[alias_not_na]['aliases'].unique())
         else:
-            return list(df_gutenberg[not_na]['author_x'].unique())
+            return list(df_gutenberg[author_not_na]['author_x'].unique())
